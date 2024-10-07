82069d3b41793dfd59314d25a93ec504ab1de118
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2024-10-07 09:46:02 +0200

    creating routes and firsts pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+// Importer les composants de pages
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Credits from './pages/Credits';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* Liens de navigation */}
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/credits">Credits</Link></li>
+          </ul>
+        </nav>
+
+        {/* Définir les Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/credits" element={<Credits />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
