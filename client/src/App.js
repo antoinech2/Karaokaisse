003bf09054ef46e0d27ddbd14fa30a1f5352b5b5
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2024-11-02 18:53:50 +0100

    refactor(client): Update routing in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-
-// Importer les composants de pages
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './helpers/AuthContext';
+import ProtectedRoute from './components/PrivateRoute';
+import First from './pages/First';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import Register from './pages/Register';
+import Login from './pages/Login';
 import Credits from './pages/Credits';
 import VideoPlayer from './pages/VideoPlayer';
 import SearchPage from './pages/Research';
@@ -11,21 +14,25 @@ import SongsListPage from './pages/SongsListPage';
 
 function App() {
   return (
-    <Router>
-      <div>
-        {/* Définir les Routes */}
+    <AuthProvider>
+      <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<First />} />
           <Route path="/credits" element={<Credits />} />
-          <Route path="/video-player" element={<VideoPlayer />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/waiting-songs" element={<SongsListPage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+
+          {/* Utiliser ProtectedRoute pour les pages nécessitant une authentification */}
+          <Route path="/home" element={<ProtectedRoute component={Home} />} />
+          <Route path="/search" element={<ProtectedRoute component={SearchPage} />} />
+          <Route path="/waiting-songs" element={<ProtectedRoute component={SongsListPage} />} />
+          <Route path="/video-player" element={<ProtectedRoute component={VideoPlayer} />} />
+
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </div>
-    </Router>
+      </Router>
+    </AuthProvider>
   );
 }
 
 export default App;
-
